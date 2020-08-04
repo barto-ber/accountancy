00670ac259a9a981a8c98aaff77be53e50f1f1de
barto-ber <58219573+barto-ber@users.noreply.github.com>
2020-08-04 15:07:26 +0200

    Delete datev2018.py

--- datev2018.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from pandas import read_csv
-pd.options.display.width = 0
-pd.options.display.max_rows = None
-# pd.options.display.precision = 2
-
-df = read_csv('susa2018.txt', header=0, sep=';')
-print(df.info())
-print(df.head())
